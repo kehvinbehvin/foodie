94e819bab2f6762addc17001607d4ea8b7c7a4c9
Kevin Ho <hokevin78@gmail.com>
2021-07-02 11:02:52 +0800

    Refactored useEffect to resolve compiler warnings

--- src/Components/API.jsx
+++ src/Components/API.jsx
@@ -1,24 +1,27 @@
-import React, { useEffect, useState } from "react";
+import React from "react";
 
 const API = () => {
-  const [recipeData, setData] = React.useState("");
   const [recipeDetails, setDetails] = React.useState("");
+  const condition = React.useRef(true);
 
   React.useEffect(() => {
-    if (recipeData === undefined) {
+    console.log(condition.current);
+    if (recipeDetails === undefined) {
       return undefined;
+    } else if (condition.current) {
+      const API_KEY = process.env.REACT_APP_APIKEY2;
+      fetch(
+        `https://api.spoonacular.com/recipes/654959/information?apiKey=${API_KEY}&query=pasta`
+      )
+        .then((res) => res.json())
+        .then((data) => {
+          condition.current = false;
+          return setDetails(data);
+        });
+    } else {
+      return null;
     }
-    // fetch(
-    //   "https://api.spoonacular.com/recipes/complexSearch?apiKey=bda294c29253473db693d0876f3c443a&query=pasta"
-    // )
-    //   .then((res) => res.json())
-    //   .then((data) => setData(data));
-    fetch(
-      "https://api.spoonacular.com/recipes/654959/information?apiKey=bda294c29253473db693d0876f3c443a&query=pasta"
-    )
-      .then((res) => res.json())
-      .then((data) => setDetails(data));
-  }, []);
+  });
 
   console.log(recipeDetails);
   return <div>API CALL</div>;
